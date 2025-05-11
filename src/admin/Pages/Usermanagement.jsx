4f9ae959204b8692e605b9f8920a56eaefc886b4
rohanmgr130 <rohanmgr130@gmail.com>
2025-05-11 11:11:59 +0545

    UserManagement part getuser

---
+++ src/admin/Pages/Usermanagement.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserManagement from '../component/Usermanage/UserManagement'
+import Nav from '../component/Nav'
+
+
+function Usermanagement() {
+  return (
+    <div>
+        <Nav/>
+        <UserManagement/>
+    </div>
+  )
+}
+
+export default Usermanagement
