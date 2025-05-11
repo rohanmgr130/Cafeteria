1c66a50d8656d1fd9ee4426839ee9da079db6052
rohanmgr130 <rohanmgr130@gmail.com>
2025-05-11 10:51:47 +0545

    Auth Login/Register of all

--- src/auth/components/LoginPage.jsx
+++ src/auth/components/LoginPage.jsx
@@ -22,7 +22,7 @@ const LoginPage = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:4000/api/login", {
+      const response = await fetch("http://localhost:4000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
